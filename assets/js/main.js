7ef70ef608d4bc13fa0433fefb25f928489b7775
Sarowar-Jahan <mdsarowarjahan3@gmail.com>
2024-09-26 01:28:38 +0600

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,16 @@
 /***************************************************
 ==================== JS INDEX ======================
 ****************************************************
-01. Sticky
-02. Preloader
-03. Magnific Image popup
-04. Text Slider
-05. Smooth active
-06. Counter active
-07. Progress Bar
-08. Scroll Top
-09. meanmenu active
-10. Button Hover Animation
-11. Pin Active
-12. Image Reveal Animation
-13. Sidebar Menu
-14. Offcanvas Menu Off/On
-15. Blog Share Pin
-16. Switcher JS
 
-
+// sticky header 
+// Smooth active
+// Preloader
+// Side Info Js
+// meanmenu activation 
+// Register GSAP Plugins
+// Counter active
+// testimonial slider
+// wow js activation 
 
 
 ****************************************************/
@@ -27,7 +19,7 @@
   "use strict";
 
 
-  // 01. Sticky 
+  // sticky header 
   let header = document.querySelector('.header-sticky');
   window.addEventListener('scroll', () => {
     if (window.scrollY > 150) {
@@ -37,23 +29,26 @@
     }
   })
 
-  // // Sticky Header Js
-  // windowOn.on("scroll", function () {
-  //   var scroll = $(window).scrollTop();
-  //   if (scroll < 100) {
-  //     $("#header-sticky").removeClass("sticky");
-  //   } else {
-  //     $("#header-sticky").addClass("sticky");
-  //   }
-  // });
 
 
-  // Screen Width
+  // Smooth active
   var device_width = window.screen.width;
-  var overlay = document.querySelector('.overlay-switcher-close');
+
+  if (device_width > 767) {
+    if (document.querySelector("#has_smooth").classList.contains("has-smooth")) {
+      const smoother = ScrollSmoother.create({
+        smooth: 0.5,
+        effects: device_width < 1025 ? false : true,
+        smoothTouch: 0.1,
+        normalizeScroll: false,
+        ignoreMobileResize: true,
+      });
+    }
+
+  }
 
 
-  // 02. Preloader
+  // Preloader
   $(document).ready(function () {
     $('#container').addClass('loaded');
     if ($('#container').hasClass('loaded')) {
@@ -87,93 +82,8 @@
   gsap.registerPlugin(ScrollTrigger);
 
 
-  // 03. Magnific Image popup
-  if ($('.image-popup').length && 'magnificPopup' in jQuery) {
-    $('.image-popup').magnificPopup({
-      type: "image",
-      gallery: {
-        enabled: true,
-      },
-    });
-  }
 
-  // Magnific Video popup
-  if ($('.video-popup').length && 'magnificPopup' in jQuery) {
-    $('.video-popup').magnificPopup({
-      type: 'iframe',
-    });
-
-  }
-
-
-  // 04. Text Slider
-  var text_slider_option = document.querySelector(".text__slider");
-
-  if (text_slider_option) {
-
-    var text_slider_speed = 5000
-    var text_slider_autoplay = true
-    var loop_value = true
-    var data_itemshow = "auto"
-
-    if (text_slider_option.getAttribute("data-sliderSpeed")) {
-      text_slider_speed = parseInt(text_slider_option.getAttribute("data-sliderSpeed"));
-    }
-    if (text_slider_option.getAttribute("data-autoPlay")) {
-      text_slider_autoplay = text_slider_option.getAttribute("data-autoPlay")
-    }
-
-    if (text_slider_option.getAttribute("data-loop")) {
-      loop_value = text_slider_option.getAttribute("data-loop")
-    }
-    if (text_slider_option.getAttribute("data-itemShow")) {
-      data_itemshow = text_slider_option.getAttribute("data-itemShow")
-    }
-
-
-    if (text_slider_autoplay == 'true') {
-      var text_slider = new Swiper(".text__slider", {
-        loop: loop_value,
-        speed: text_slider_speed,
-        allowTouchMove: false,
-        slidesPerView: data_itemshow,
-        slidesPerGroup: 10,
-        spaceBetween: 20,
-        autoplay: {
-          delay: 0,
-          disableOnInteraction: true,
-        }
-      });
-    }
-    else {
-      var text_slider = new Swiper(".text__slider", {
-        loop: loop_value,
-        speed: text_slider_speed,
-        allowTouchMove: false,
-        slidesPerView: data_itemshow,
-        slidesPerGroup: 10,
-        spaceBetween: 20,
-        autoplay: false,
-      });
-    }
-  };
-
-
-  // 05. Smooth active
-  if (device_width > 767) {
-    if (document.querySelector("#has_smooth").classList.contains("has-smooth")) {
-      const smoother = ScrollSmoother.create({
-        smooth: 0.5,
-        effects: device_width < 1025 ? false : true,
-        smoothTouch: 0.1,
-        normalizeScroll: false,
-        ignoreMobileResize: true,
-      });
-    }
-
-  }
-
-  // 06. Counter active
+  // Counter active
   if ('counterUp' in window) {
     const skill_counter = window.counterUp.default
     const skill_cb = entries => {
@@ -199,245 +109,6 @@
       IO.observe(el)
     });
   }
-
-
-  // 07. Progress Bar
-  if ($('.progress__label').length) {
-    $('.progress__label').progressBar({
-      percentage: true,
-      animation: true,
-      height: "2",
-      barColor: "#E8BF96",
-      shadow: true,
-    });
-  }
-
-  if ($('.progress__label-2').length) {
-    $('.progress__label-2').progressBar({
-      height: "2",
-      percentage: true,
-      animation: true,
-      shadow: true,
-      barColor: "#FD8F77",
-      backgroundColor: "#44454A",
-    });
-
-    var progress_item_2 = document.querySelectorAll('.progress__item-2 .sonny_progressbar');
-    if (progress_item_2) {
-      progress_item_2.forEach(elem => {
-        var prog_val = elem.getAttribute('data-width');
-        elem.childNodes[1].childNodes[2].style.left = prog_val + '%';
-      });
-    }
-
-  }
-
-
-  // 08. Scroll Top
-  let scroll_top = document.getElementById("scroll_top");
-  if (scroll_top) {
-    window.onscroll = function () {
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        scroll_top.classList.add('showed');
-      } else {
-        scroll_top.classList.remove('showed');
-      }
-    };
-
-    scroll_top.addEventListener('click', function () {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    });
-  }
-
-
-
-
-
-
-  const all_btn = gsap.utils.toArray(".btn-move");
-  const all_btn_cirlce = gsap.utils.toArray(".btn-item");
-
-  all_btn.forEach((btn, i) => {
-    $(btn).mousemove(function (e) {
-      callParallax(e);
-    });
-    function callParallax(e) {
-      parallaxIt(e, all_btn_cirlce[i], 80);
-    }
-
-    function parallaxIt(e, target, movement) {
-      var $this = $(btn);
-      var relX = e.pageX - $this.offset().left;
-      var relY = e.pageY - $this.offset().top;
-
-      gsap.to(target, 0.3, {
-        x: ((relX - $this.width() / 2) / $this.width()) * movement,
-        y: ((relY - $this.height() / 2) / $this.height()) * movement,
-        scale: 1.2,
-        ease: Power2.easeOut,
-      });
-    }
-    $(btn).mouseleave(function (e) {
-      gsap.to(all_btn_cirlce[i], 0.3, {
-        x: 0,
-        y: 0,
-        scale: 1,
-        ease: Power2.easeOut,
-      });
-    });
-  });
-
-  // 10. Button Hover Animation
-  var btn_hover_all = document.querySelectorAll(".btn-hover-bgchange");
-
-  if (btn_hover_all) {
-    for (const ele of btn_hover_all) {
-      var newSpan = document.createElement("span");
-      ele.appendChild(newSpan);
-    }
-
-    $('.btn-hover-bgchange').on('mouseenter', function (e) {
-      var x = e.pageX - $(this).offset().left;
-      var y = e.pageY - $(this).offset().top;
-
-      $(this).find('span').css({
-        top: y,
-        left: x,
-      });
-    });
-
-    $('.btn-hover-bgchange').on('mouseout', function (e) {
-      var x = e.pageX - $(this).offset().left;
-      var y = e.pageY - $(this).offset().top;
-
-      $(this).find('span').css({
-        top: y,
-        left: x,
-      });
-    });
-  }
-
-
-  // 11. Pin Active
-  var pin_fixed = document.querySelector('.pin__element');
-  if (pin_fixed && device_width > 991) {
-
-    gsap.to(".pin__element", {
-      scrollTrigger: {
-        trigger: ".pin__area",
-        pin: ".pin__element",
-        start: "top top",
-        end: "bottom bottom",
-        pinSpacing: false,
-      }
-    });
-  }
-
-  var schedule_fixed = document.querySelector('.pin__elem');
-  if (schedule_fixed && device_width > 991) {
-
-    gsap.utils.toArray(".pin__elem").forEach((panel, i, array) => {
-
-      if (i === array.length - 1) {
-        return;
-      }
-
-      ScrollTrigger.create({
-        trigger: panel,
-        start: "top top",
-        pin: true,
-        pinSpacing: false
-      });
-
-    });
-
-
-  }
-
-
-
-
-
-  // 12. Image Reveal Animation
-  let img_anim_reveal = document.querySelectorAll(".img_anim_reveal");
-
-  img_anim_reveal.forEach((img_reveal) => {
-    let image = img_reveal.querySelector("img");
-    let tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: img_reveal,
-        start: "top 50%",
-      }
-    });
-
-    tl.set(img_reveal, { autoAlpha: 1 });
-    tl.from(img_reveal, 1.5, {
-      xPercent: -100,
-      ease: Power2.out
-    });
-    tl.from(image, 1.5, {
-      xPercent: 100,
-      scale: 1.3,
-      delay: -1.5,
-      ease: Power2.out
-    });
-  });
-
-
-
-
-  // 14. Offcanvas Menu Off/On
-  var open_offcanvas = document.getElementById("open_offcanvas");
-  var close_offcanvas = document.getElementById("close_offcanvas");
-
-  if (open_offcanvas && close_offcanvas) {
-    open_offcanvas.addEventListener('click', function () {
-      document.querySelector('.offcanvas-area').style.opacity = '1';
-      document.querySelector('.offcanvas-area').style.visibility = 'visible';
-    });
-
-    close_offcanvas.addEventListener('click', function () {
-      document.querySelector('.offcanvas-area').style.opacity = '0';
-      document.querySelector('.offcanvas-area').style.visibility = 'hidden';
-    });
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-  $("a[href='#top']").on('click', function () {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
-
-
-
-
-  const toggle_switcher = function () {
-
-    let $scope = $('.wcf__toggle_switcher');
-
-    const checked = $("input", $scope);
-    const toggle_pane = $(".toggle-pane", $scope);
-    const toggle_label = $(".before_label, .after_label", $scope);
-
-    checked.change(function () {
-      toggle_pane.toggleClass('show');
-      toggle_label.toggleClass('active');
-    })
-  }
-
-  toggle_switcher();
-
 
 
   // testimonial slider
@@ -478,7 +149,7 @@
     });
   }
 
-
-
+  // wow js activation 
+  new WOW().init();
 
 })(jQuery);
